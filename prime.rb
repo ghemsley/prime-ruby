921790a9ef0f8cd50949daa98da0e37f14f07e6c
ghemsley <ghemsley@protonmail.ch>
2020-10-22 14:04:52 -0500

    commit tree 77652de6755fdcfea334177c9d1fe23502f16914

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+  return false if (-1..1).include?(number)
+  boolean = (2..(number - 1)).find do |iteration|
+    (number % iteration).zero?
+  end
+  boolean.nil? ? true : false
+end
